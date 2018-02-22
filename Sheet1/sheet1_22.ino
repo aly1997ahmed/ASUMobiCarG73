e340ead6a6f601b38deeecd253e223b0f4460700
menna2mohamed <36630788+menna2mohamed@users.noreply.github.com>
2018-02-22 15:18:15 +0200

    Add files via upload

---
+++ Sheet1/sheet1_22.ino
@@ -0,0 +1,52 @@
+
+int leda = 4;
+int ledb = 5;
+int ledc = 6;
+int ledd = 7;
+int i,j ;
+
+void setup()
+{
+ pinMode (leda , OUTPUT );
+ pinMode (ledb , OUTPUT ); 
+ pinMode (ledc , OUTPUT );
+ pinMode (ledd , OUTPUT );
+
+}
+
+void loop()
+{
+  digitalWrite (leda,HIGH);
+  digitalWrite (ledb,LOW);
+  digitalWrite (ledc,LOW);
+  digitalWrite (ledd,LOW);
+  delay(1000);
+  digitalWrite (ledb,HIGH);
+  digitalWrite (leda,LOW);
+  digitalWrite (ledc,LOW);
+  digitalWrite (ledd,LOW);
+  delay(1000);
+  digitalWrite (ledc,HIGH);
+  digitalWrite (leda,LOW);
+  digitalWrite (ledb,LOW);
+  digitalWrite (ledd,LOW);
+  delay(1000);
+  digitalWrite (ledd,HIGH);
+  digitalWrite (leda,LOW);
+  digitalWrite (ledb,LOW);
+  digitalWrite (ledc,LOW);
+  delay(1000);
+  digitalWrite (ledc,HIGH);
+  digitalWrite (leda,LOW);
+  digitalWrite (ledb,LOW);
+  digitalWrite (ledd,LOW);
+  delay(1000);
+  digitalWrite (ledb,HIGH);
+  digitalWrite (leda,LOW);
+  digitalWrite (ledb,LOW);
+  digitalWrite (ledc,LOW);
+  delay(1000);
+  
+ }
+
+
